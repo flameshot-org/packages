ebed750ec72e79bb90917eac700a252fc0431ef5
Ahmed Zetao Yang <zetao.yang@hotmail.com>
2018-12-29 21:25:55 +0800

    add docker '--privileged' flag

--- scripts/flatpak_before_install.sh
+++ scripts/flatpak_before_install.sh
@@ -3,4 +3,4 @@
 set -e
 
 cp -rf "${ROOT_PATH}"/flatpak "${SRC_PATH}"
-docker run --name flatpak-builder -v $SRC_PATH:$SRC_PATH -w $SRC_PATH -td $FLATPAK_DOCKER_REPO /bin/bash
+docker run --privileged --name flatpak-builder -v $SRC_PATH:$SRC_PATH -w $SRC_PATH -td $FLATPAK_DOCKER_REPO /bin/bash
