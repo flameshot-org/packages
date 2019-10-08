e4fa69a0c231d948b03212041106ad226063557a
Ahmed Zetao Yang <zetao.yang@hotmail.com>
2019-10-08 13:51:36 +0800

    feat: flatpak packaging

--- scripts/flatpak_script.sh
+++ scripts/flatpak_script.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+DIST_PATH="${SRC_PATH}"/dist
+mkdir "${DIST_PATH}"
+
 if [[ "${RUN_TIME}" == "gnome" ]]; then 
 	docker exec -i flatpak-builder sh -c "flatpak-builder build ${SRC_PATH}/flatpak/gnome/org.dharkael.Flameshot.yaml" 
 elif [[ "${RUN_TIME}" == "kde" ]]; then 
-	docker exec -i flatpak-builder sh -c "flatpak-builder build ${SRC_PATH}/flatpak/kde/org.dharkael.Flameshot.yaml" 
+	docker exec -i flatpak-builder sh -c "flatpak-builder --repo=repo --force-clean flameshot-app-dir ${SRC_PATH}/flatpak/kde/org.dharkael.Flameshot.yaml" 
+	docker exec -i flatpak-builder sh -c "flatpak --user remote-add --no-gpg-verify flameshot-repo repo" 
+	docker exec -i flatpak-builder sh -c "flatpak build-bundle ${SRC_PATH}/repo flameshot.flatpak org.dharkael.Flameshot --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo"
+	docker exec -i flatpak-builder sh -c "cp ${SRC_PATH}/flameshot.flatpak ${DIST_PATH}/flameshot_${VERSION}_${ARCH}.${EXTEN}"
 elif [[ "${RUN_TIME}" == "freedesktop" ]]; then 
 	docker exec -i flatpak-builder sh -c "flatpak-builder build ${SRC_PATH}/flatpak/freedesktop/org.dharkael.Flameshot.yaml" 
 fi
