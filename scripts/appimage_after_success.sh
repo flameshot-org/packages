a2456a4790ea345315e0aaf72f64f394acbae8ba
Ahmed Zetao Yang <zetao.yang@hotmail.com>
2019-10-08 09:22:11 +0800

    feat: update upload services

--- scripts/appimage_after_success.sh
+++ scripts/appimage_after_success.sh
@@ -7,4 +7,8 @@ echo "File name: flameshot_${VERSION}_${ARCH}.${EXTEN} ."
 SHA256=$(sha256sum "${DIST_PATH}/flameshot_${VERSION}_${ARCH}.${EXTEN}")
 echo "SHA256: ${SHA256} ."
 
-travis_retry bash "${ROOT_PATH}/scripts/services/0x0.st.sh" "${DIST_PATH}/flameshot_${VERSION}_${ARCH}.${EXTEN}"
+# https://0x0.st
+# travis_retry bash "${ROOT_PATH}/scripts/services/0x0.st.sh" "${DIST_PATH}/flameshot_${VERSION}_${ARCH}.${EXTEN}"
+
+# https://wetransfer.com
+travis_retry bash "${ROOT_PATH}/scripts/services/wetransfer.com.sh" "${DIST_PATH}/flameshot_${VERSION}_${ARCH}.${EXTEN}"
--- scripts/snap_after_success.sh
+++ scripts/snap_after_success.sh
@@ -15,4 +15,8 @@ echo "SHA256: ${SHA256} ."
 
 timeout 240 /snap/bin/transfer "$RESULT_SNAP"
 
-travis_retry bash "${ROOT_PATH}/scripts/services/0x0.st.sh" "${RESULT_SNAP}"
+# https://0x0.st
+# travis_retry bash "${ROOT_PATH}/scripts/services/0x0.st.sh" "${RESULT_SNAP}"
+
+# https://wetransfer.com
+travis_retry bash "${ROOT_PATH}/scripts/services/wetransfer.com.sh" "${RESULT_SNAP}"
